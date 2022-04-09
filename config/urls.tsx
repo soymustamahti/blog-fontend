12168547322c4936b133cb01df2cdf26b5688dbb
Mustapha El Hachmi Mahti <musta@MacBook-Pro-de-Mustapha.local>
2022-04-09 16:43:34 +0200

    Edite profile create profile profile ok

--- config/urls.tsx
+++ config/urls.tsx
@@ -3,3 +3,6 @@ const dev = process.env.NODE_ENV !== 'production';
 export const server = dev
     ? 'http://localhost:3001'
     : 'https://blog-musta.herokuapp.com';
+
+export const routes = dev ? 'http://localhost:3000'
+    : 'https://blog-musta-frontend.herokuapp.com';
---
+++ pages/profile/thanks_creation.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import Thanks from '../../components/messages/Thanks';
+import Title from '../../components/Title';
+
+export default function thanks() {
+    return (
+        <>
+            <Layout>
+                <Title title={'Apply'}/>
+
+                <Thanks
+                    msgOne={
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, sequi'
+                    }
+                    msgTwo={
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, sequi'
+                    }
+                    msgThree={
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, sequi'
+                    }
+                />
+            </Layout>
+        </>
+    );
+}
